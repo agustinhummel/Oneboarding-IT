0817fc3ef6bff52e367901037efa816a4643be40
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-08-22 14:15:25 -0300

    Se leen modelos

--- config/index.js
+++ config/index.js
@@ -33,7 +33,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(modelsDir, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
