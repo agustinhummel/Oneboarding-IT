f5738e4775deab707c256206e8d082599ec17922
agustin hummel <104798688+agustinhummel@users.noreply.github.com>
2023-08-22 14:06:52 -0300

    create model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+    }
+  };
+  User.init({
+    userName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  }, {
+    sequelize,
+    paranoid: true,
+    timestamps: true,
+    modelName: 'User',
+  })
+  return User;
+};
