454d20241e7cc723330b185e9b8e2033eb84ab26
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-08-25 18:21:45 -0300

    Se agregan mas dependencias y el middleware de las rutas de users

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const express=require('express');
 const app=express();
+const sequelizeConfig=require('./config/index');
+const morgan=require('morgan');
 require('dotenv').config();
+const userRoute=require('./routes/userRoute');
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use('/users',userRoute);
 
 const port=process.env.PORT;
-app.listen(port, ()=>{
-    console.log(`Servidor en el puerto ${port}`);
+
+sequelizeConfig.authenticateAndSync()
+.then(()=>{
+    sequelizeConfig.loadModels();
+    app.listen(port, ()=>{
+        console.log(`Servidor en el puerto ${port}`);
+    });
+})
+.catch((error)=>{
+    console.log(error.message);
 });
