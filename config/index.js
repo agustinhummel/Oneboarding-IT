7577b24f523fac40d6a4e633bbbc95b3ff68cae9
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-08-25 18:18:27 -0300

    Ahora se exportan dos funciones para crear la conexión y cargar los modelos

--- config/index.js
+++ config/index.js
@@ -1,49 +1,49 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const conexionObj=require('./conexion');
+const conexionObj = require('./conexion');
 const db = {};
 
-let sequelize;
-sequelize = new Sequelize(conexionObj.database, conexionObj.user, conexionObj.password, conexionObj);
-sequelize.authenticate()
-.then(()=>{
-  console.log('Conectado a la db');
-})
-.catch((error)=>{
-  console.log(error.message);
-});
 
-sequelize.sync()
-  .then(()=>{
-    console.log('Cambios reflejados');
-  })
-  .catch((error)=>{
-    console.log(error.message);
+const sequelize = new Sequelize(conexionObj.database, conexionObj.user, conexionObj.password, conexionObj);
+const authenticateAndSync = () => {
+  return sequelize.authenticate()
+    .then(() => {
+      console.log('Conectado a la db');
+      return sequelize.sync();
+    })
+    .then(() => {
+      console.log('Cambios reflejados');
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+const loadModels = () => {
+  const modelsDir = path.join(__dirname, '../models');
+  fs
+    .readdirSync(modelsDir)
+    .filter(file => {
+      return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+      const model = require(path.join(modelsDir, file))(sequelize, Sequelize.DataTypes);
+      db[model.name] = model;
+    });
+
+  Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
   });
 
+  return db;
+};
 
-const modelsDir = path.join(__dirname, '../models');
-fs
-  .readdirSync(modelsDir)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(modelsDir, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = {
+  sequelize,
+  authenticateAndSync,
+  loadModels
+};
