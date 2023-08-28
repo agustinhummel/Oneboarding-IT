1069c2d179585c79e561a9e6d0b08526a992463e
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-08-28 15:37:49 -0300

    middleware json de express

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 const express=require('express');
 const app=express();
-const sequelizeConfig=require('./config/index');
 const morgan=require('morgan');
 require('dotenv').config();
 const userRoute=require('./routes/userRoute');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use('/users',userRoute);
 
 const port=process.env.PORT;
 
-sequelizeConfig.authenticateAndSync()
-.then(()=>{
-    sequelizeConfig.loadModels();
-    app.listen(port, ()=>{
-        console.log(`Servidor en el puerto ${port}`);
-    });
+app.listen(port, ()=>{
+    console.log(`Servidor en el puerto ${port}`);
 })
-.catch((error)=>{
-    console.log(error.message);
-});
