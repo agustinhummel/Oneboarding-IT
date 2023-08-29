f1e3d12cbb6c457fb8de31a3805b84a6381fd57e
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-08-29 15:52:59 -0300

    ruta clients

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const app=express();
 const morgan=require('morgan');
 require('dotenv').config();
 const userRoute=require('./routes/userRoute');
+const clientRoute=require('./routes/clientRoute');
+
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use('/users',userRoute);
+app.use('/clients',clientRoute);
 
-const port=process.env.PORT;
-
+const port=process.env.PORT || 8080;
 app.listen(port, ()=>{
     console.log(`Servidor en el puerto ${port}`);
 })
