232952f6ea9b4792427b3bcf0ccd4f6b949ac515
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-09-27 13:44:28 -0300

    model

--- models/Post.js
+++ models/Post.js
@@ -1,23 +1,39 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize) => {
   class Vacantes extends Model {
     static associate(models) {
-      
+
     }
   }
   Vacantes.init(
     {
       skills: DataTypes.STRING(500),
       descripcion: DataTypes.STRING(3000),
+      fecha_alta: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      fecha_modif: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
     },
     {
       sequelize,
       paranoid: true,
-      timestamps: true,
+      timestamps: false,
       modelName: "Vacantes",
     }
   );
+
+  // Definir un gancho (hook) antes de actualizar
+  Vacantes.beforeUpdate((vacante, options) => {
+    vacante.fecha_modif = new Date();
+  });
+
   return Vacantes;
 };
